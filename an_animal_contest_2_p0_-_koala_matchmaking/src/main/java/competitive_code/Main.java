ae4a19438b5acfaaf3e15316fad52450d7818e5c
MrChaos <mrchaos1213@gmail.com>
2024-02-01 12:15:43 +0100

    number to high?

---
+++ an_animal_contest_2_p0_-_koala_matchmaking/src/main/java/competitive_code/Main.java
@@ -0,0 +1,27 @@
+package competitive_code;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main v = new Main();
+        v.run();
+    }
+
+    public void run() throws IOException {
+        System.out.println(matchmaking());
+    }
+
+    public String matchmaking() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long favouriteNumber = Long.parseLong(st.nextToken());
+
+        long average = Math.round((favouriteNumber + (favouriteNumber - 2)) / 2);
+
+        return String.valueOf(average);
+    }
+}
